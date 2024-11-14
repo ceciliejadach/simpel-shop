3e713602bf6dcbf532d44c1c50d95d75458f81e4
”sagecave” <Andreassage@gmail.com>
2024-11-14 19:44:16 +0100

    total pris virker næsten

--- src/app/components/List.jsx
+++ src/app/components/List.jsx
@@ -1,13 +1,34 @@
+"use client";
 import ListItem from "./ListItem";
-
+import { useState } from "react";
 const List = ({ items, setItems, deleteItem, basketCounter }) => {
+  const [productCount, setProductCount] = useState(1);
   <ListItem items={items} setItems={setItems}></ListItem>;
+
+  const totalPrice = items.reduce((prePrice, items) => prePrice + items.price, 0);
+  console.log(totalPrice, "prisen er");
+
   return (
-    <ul>
-      {items.map((items) => (
-        <ListItem items={items} basketCounter={basketCounter} key={items.id} title={items.text} price={items.price} tag={items.tag} image={items.image} itemId={items.id} deleteItem={deleteItem}></ListItem>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {items.map((items) => (
+          <ListItem
+            setProductCount={setProductCount}
+            productCount={productCount}
+            items={items}
+            basketCounter={basketCounter}
+            key={items.id}
+            title={items.text}
+            price={items.price}
+            tag={items.tag}
+            image={items.image}
+            itemId={items.id}
+            deleteItem={deleteItem}
+          ></ListItem>
+        ))}
+      </ul>
+      <div>{items?.length > 0 && <p>{`Total Pris ${totalPrice}`}</p>}</div>
+    </div>
   );
 };
 

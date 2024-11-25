f2731ae53b2b3cea55ee6c8329cf93dfc134470d
laerkehansen <laha0007@stud.kea.dk>
2024-11-25 11:11:06 +0100

    stylet singleview

--- src/app/components/ProductSingle.jsx
+++ src/app/components/ProductSingle.jsx
@@ -5,16 +5,42 @@ import Accordion from "./Accordion";
 import ReviewContainer from "./ReviewContainer";
 
 const ProductSingle = ({ product }) => {
-  const { title, brand, thumbnail, images, description, price, stock, dimensions, warrantyInformation, shippingInformation, sku, reviews } = product;
+  const {
+    title,
+    brand,
+    thumbnail,
+    images,
+    description,
+    price,
+    stock,
+    dimensions,
+    warrantyInformation,
+    shippingInformation,
+    sku,
+    reviews,
+  } = product;
   return (
     <>
-      <Gallery thumbnail={thumbnail} images={images} title={title} />
-      <p className="pb-3xs text-medium">{brand}</p>
-      <p className="text-subtitle">{title}</p>
-      <p className="max-w-[55ch] my-xs">{description}</p>
-      <p>{stock}</p>
-      <Accordion dimensions={dimensions} warrantyInformation={warrantyInformation} shippingInformation={shippingInformation} sku={sku} />
-      <ReviewContainer reviews={reviews} />
+      <section className="max-[800px]:grid-cols-[0.1fr_1fr_0.1fr] grid">
+        <div className="max-[800px]:col-start-2 min-[801px]:grid-cols-[0.1fr_1fr_1fr_0.1fr] grid items-center justify-items-center">
+          <div className="min-[801px]:col-start-2 w-fit align-middle">
+            <Gallery thumbnail={thumbnail} images={images} title={title} />
+          </div>
+          <div className="min-[801px]:col-start-3 w-fit">
+            <p className="pb-3xs font-semibold text-4xl md:text-5xl">{brand}</p>
+            <p className="text-subtitle pb-6 text-gray-600 text-2xl">{title}</p>
+            <p className="max-w-[55ch] my-xs text-xl">{description}</p>
+            <p className="py-4 text-xl">Antal tilbage på lager: {stock}</p>
+            <Accordion
+              dimensions={dimensions}
+              warrantyInformation={warrantyInformation}
+              shippingInformation={shippingInformation}
+              sku={sku}
+            />
+          </div>
+          <ReviewContainer reviews={reviews} />
+        </div>
+      </section>
     </>
   );
 };
--- src/app/components/ReviewContainer.jsx
+++ src/app/components/ReviewContainer.jsx
@@ -7,31 +7,43 @@ import { FaStarHalfAlt } from "react-icons/fa";
 const ReviewContainer = ({ reviews }) => {
   return (
     <>
-      <h2 className="text-center">Reviews</h2>
-      <ul className="flex gap-2 justify-center my-8 flex-wrap">
-        {reviews.map((review, id) => (
-          <li key={id} className="bg-[#EFEEFF] rounded-md h-fit w-58 grid gap-3 p-4  max-[400px]:w-44">
-            <div className="grid gap-4">
-              <h2 className="text-xl text-black">
-                <strong>{review.reviewerName}</strong>
-              </h2>
-              <p className="text-black">{review.comment}</p>
-            </div>
-            <div className="flex gap-1 justify-self-center">
-              {Array.from({ length: 5 }, (_, index) => {
-                if (index < Math.floor(review.rating)) {
-                  return <FaStar key={index} color="#3C35FF" size={20} />;
-                } else if (index < Math.floor(review.rating) + 1 && review.rating % 1 >= 0.5) {
-                  return <FaStarHalfAlt key={index} color="#3C35FF" size={20} />;
-                } else {
-                  return <FaRegStar key={index} color="#3C35FF" size={20} />;
-                }
-              })}
-            </div>
-            <p className="justify-self-end">{new Date(review.date).toLocaleDateString()}</p>
-          </li>
-        ))}
-      </ul>
+      <section className="px-2 min-[801px]:col-start-2 min-[801px]:col-end-4">
+        <h2 className="text-center font-semibold pt-6 text-4xl">Reviews</h2>
+        <ul className="flex gap-4 justify-center my-8 flex-wrap">
+          {reviews.map((review, id) => (
+            <li
+              key={id}
+              className="bg-[#EFEEFF] rounded-md h-fit min-[401px]:w-60 min-[500px]:p-6 grid gap-3 p-6  max-[400px]:w-44"
+            >
+              <div className="grid gap-4">
+                <h2 className="text-xl text-black">
+                  <strong>{review.reviewerName}</strong>
+                </h2>
+                <p className="text-black">{review.comment}</p>
+              </div>
+              <div className="flex gap-1 justify-self-center">
+                {Array.from({ length: 5 }, (_, index) => {
+                  if (index < Math.floor(review.rating)) {
+                    return <FaStar key={index} color="#3C35FF" size={20} />;
+                  } else if (
+                    index < Math.floor(review.rating) + 1 &&
+                    review.rating % 1 >= 0.5
+                  ) {
+                    return (
+                      <FaStarHalfAlt key={index} color="#3C35FF" size={20} />
+                    );
+                  } else {
+                    return <FaRegStar key={index} color="#3C35FF" size={20} />;
+                  }
+                })}
+              </div>
+              <p className="justify-self-end">
+                {new Date(review.date).toLocaleDateString()}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 };

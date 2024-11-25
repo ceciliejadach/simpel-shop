e5feaf7e547fcb262f4926316d8626ce2163eae3
”sagecave” <Andreassage@gmail.com>
2024-11-25 11:43:04 +0100

    styling done

--- src/app/components/PrimaryButton.jsx
+++ src/app/components/PrimaryButton.jsx
@@ -1,6 +1,19 @@
 const PrimaryButton = ({ btntext, theme = "default", onClick }) => {
-  const themes = theme === "red" ? "bg-accent " : theme === "white" ? "bg-[--background] text-[--foreground] outline outline-2 hover:outline-none" : theme === "black" ? "bg-[--foreground] text-[--background]" : theme === "soldOut" ? "bg-[--foreground] text-[--background] hover:bg-[--foreground] cursor-default" : "bg-dark dark:text-[--background]"; // default
-  return <button onClick={onClick}>{btntext}</button>;
+  const themes =
+    theme === "red"
+      ? "bg-accent "
+      : theme === "white"
+      ? "bg-[--background] text-[--foreground] outline outline-2 hover:outline-none"
+      : theme === "black"
+      ? "bg-[--foreground] text-[--background]"
+      : theme === "soldOut"
+      ? "bg-[--foreground] text-[--background] hover:bg-[--foreground] cursor-default"
+      : "bg-dark dark:text-[--background]"; // default
+  return (
+    <button className="bg-[#3C35FF]  rounded-full text-white pt-2 text-center pb-2 w-max p-4 hover:bg-[#514cd3] " onClick={onClick}>
+      {btntext}
+    </button>
+  );
 };
 
 export default PrimaryButton;
